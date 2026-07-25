2834bd1479dddaf8659001cfc0ded26795698ab1
Lando Icaza <landoicco@gmail.com>
2026-07-25 06:50:29 -0600

    Docker Compose added to project  - Local deployment improved

--- core/src/main/java/licaza/etiya/core/config/DynamoDbConfig.java
+++ core/src/main/java/licaza/etiya/core/config/DynamoDbConfig.java
@@ -1,6 +1,7 @@
 package licaza.etiya.core.config;
 
 import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,11 +16,13 @@ public class DynamoDbConfig {
 
   @Bean
   @Profile("local")
-  public DynamoDbClient localDynamoDbClient() {
+  public DynamoDbClient localDynamoDbClient(
+      @Value("${aws.dynamodb.endpoint}") String endpoint,
+      @Value("${aws.region:us-east-1}") String region) {
     System.out.println("🔌 Connecting to local DynamoDB (Docker)...");
     return DynamoDbClient.builder()
-        .endpointOverride(URI.create("http://localhost:8000"))
-        .region(Region.US_EAST_1)
+        .endpointOverride(URI.create(endpoint))
+        .region(Region.of(region))
         .credentialsProvider(
             StaticCredentialsProvider.create(AwsBasicCredentials.create("local", "local")))
         .build();
