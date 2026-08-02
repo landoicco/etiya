d42039fba82aa6fd1ac6fe8e426f42ba5c03311a
Lando Icaza <landoicco@gmail.com>
2026-08-01 21:17:20 -0600

    Message logic added to GymFunctions  - Fail case added on Bruno for Gym logic

--- core/src/main/java/licaza/etiya/core/functions/GymFunctions.java
+++ core/src/main/java/licaza/etiya/core/functions/GymFunctions.java
@@ -2,11 +2,14 @@ package licaza.etiya.core.functions;
 
 import java.util.List;
 import java.util.function.Function;
+import licaza.etiya.core.exception.FunctionWrapper;
 import licaza.etiya.core.model.Gym;
 import licaza.etiya.core.service.GymService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @Slf4j
 @Configuration
@@ -19,26 +22,40 @@ public class GymFunctions {
   }
 
   @Bean
-  public Function<Gym, Gym> registerGym() {
-    return input -> {
-      log.info("📥 Request received to register a new gym: '{}'", input.getName());
+  public Function<Message<Gym>, Message<?>> registerGym() {
+    return FunctionWrapper.<Gym, Object>safe(
+        input -> {
+          Gym gymInput = input.getPayload();
 
-      Gym savedGym = service.registerGym(input);
+          log.info("📥 Request received to register a new gym: '{}'", gymInput.getName());
 
-      log.info("🏢 Gym successfully processed and saved with ID: {}", savedGym.getId());
-      return savedGym;
-    };
+          Gym savedGym = service.registerGym(gymInput);
+
+          log.info("🏢 Gym successfully processed and saved with ID: {}", savedGym.getId());
+
+          return MessageBuilder.withPayload((Object) savedGym)
+              .setHeader("statusCode", 201)
+              .setHeader("Content-Type", "application/json")
+              .build();
+        });
   }
 
   @Bean
-  public Function<String, List<Gym>> searchGyms() {
-    return query -> {
-      log.info("📥 Request received to search gyms with query: '{}'", query);
+  public Function<Message<String>, Message<?>> searchGyms() {
+    return FunctionWrapper.<String, Object>safe(
+        input -> {
+          String query = input.getPayload();
 
-      List<Gym> results = service.searchGyms(query);
+          log.info("📥 Request received to search gyms with query: '{}'", query);
 
-      log.info("✨ Search completed. Found {} gyms.", results.size());
-      return results;
-    };
+          List<Gym> results = service.searchGyms(query);
+
+          log.info("✨ Search completed. Found {} gyms.", results.size());
+
+          return MessageBuilder.withPayload((Object) results)
+              .setHeader("statusCode", 200)
+              .setHeader("Content-Type", "application/json")
+              .build();
+        });
   }
 }
